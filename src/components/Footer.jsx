2d926d11d8e15cb708f961063868729617f86365
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-30 23:25:28 +0530

    create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+     <footer className='bg-black text-white flex items-center justify-around py-8 md:text-lg'>
+        <h4>Subscribe</h4>
+        <h4>More Information</h4>
+        <h4>Media Social</h4>
+     </footer>
+  )  
+}
+
+export default Footer
