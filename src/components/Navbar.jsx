062b13d98464cb29ea1638e56322f9c0297b2d1c
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-30 23:26:12 +0530

    Add functionality on button lists

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai";
-import logo from "../assets/flashfeed-logo.png";
+import logo from "../assets/flash.png";
 
-const Navbar = () => {
+const Navbar = ({onCategoryChange}) => {
 
     const [nav, setNav] = useState(true);
 
@@ -12,14 +12,14 @@ const Navbar = () => {
     }
 
     return (
-      <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
+      <div className="text-white flex justify-between shadow-lg items-center h-24 mx-auto px-4">
         <img className="w-48" src={logo} alt="logo" />
         <ul className=" hidden md:flex items-center text-black md:font-semibold md:text-lg cursor-pointer">
-          <li className="p-4">World</li>
-          <li className="p-4">Sports</li>
-          <li className="p-4">Tech</li>
-          <li className="p-4">Science</li>
-          <li className="p-4">Entertainment</li>
+          <li className="p-4 cursor-pointer"  >World</li>
+          <li className="p-4 cursor-pointer">Sports</li>
+          <li className="p-4 cursor-pointer">Tech</li>
+          <li className="p-4 cursor-pointer">Science</li>
+          <li className="p-4 cursor-pointer">Entertainment</li>
         </ul>
         <div className="cursor-pointer text-black block md:hidden" onClick={handleNav}>
           {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
@@ -33,11 +33,11 @@ const Navbar = () => {
         >
           <img className="w-48" src={logo} alt="logo" />
           <ul className="p-4 uppercase text-black font-semibold cursor-pointer">
-            <li className="p-4 border-b border-gray-600">World</li>
-            <li className="p-4 border-b border-gray-600">Sports</li>
-            <li className="p-4 border-b border-gray-600">Tech</li>
-            <li className="p-4 border-b border-gray-600">Science</li>
-            <li className="p-4">Entertainment</li>
+            <li className="p-4 cursor-pointer border-b border-gray-600">World</li>
+            <li className="p-4 cursor-pointer border-b border-gray-600">Sports</li>
+            <li className="p-4 cursor-pointer border-b border-gray-600">Tech</li>
+            <li className="p-4 cursor-pointer border-b border-gray-600">Science</li>
+            <li className="p-4 cursor-pointer">Entertainment</li>
           </ul>
         </div>
       </div>
