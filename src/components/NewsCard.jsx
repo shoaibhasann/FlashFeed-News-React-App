5a6625e080aa940f1019220e0e268eed2a17b862
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-30 23:26:40 +0530

    Changes in Category

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 
-function NewsCard({ title, description, image, cardKey}) {
+function NewsCard({ title, description, imageUrl, cardKey, url }) {
+  const cardClassName =
+    cardKey === 0
+      ? "col-span-2  bg-white border border-black shadow p-1"
+      : cardKey === 8
+      ? "col-span-2  bg-white border border-black shadow p-1"
+      : "max-w-sm bg-white border border-black shadow p-1";
+
   return (
-    <div key={cardKey} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div key={cardKey} className={cardClassName}>
       <a href="#">
-        <img
-          className="rounded-t-lg"
-          src={image}
-          alt=""
-        />
+        <img src={imageUrl} alt="" />
       </a>
       <div className="p-5">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
             {title}
           </h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-         {description}
-        </p>
+        <p className="mb-3 font-normal text-gray-700">{description}</p>
         <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#ea504c] focus:ring-4 focus:outline-none hover:text-[#ea504c] hover:bg-white hover:border hover:border-[#ea504c]"
         >
           Read more
         </a>
